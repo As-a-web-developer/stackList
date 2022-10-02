42b74eefb172900b625b551bc925622cd5f6e3ab
Adesipe Adeola <princeadeola234@gmail.com>
2022-10-02 09:29:36 -0700

    committed

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius in labore temporibus eveniet accusantium atque alias minus, sapiente voluptate aperiam veniam illo ducimus mollitia tempora delectus! Perferendis aliquam dolore eos.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius in labore temporibus eveniet accusantium atque alias minus, sapiente voluptate aperiam veniam illo ducimus mollitia tempora delectus! Perferendis aliquam dolore eos.</p>
+        </div>
+     );
+}
+ 
+export default About;
---
+++ pages/stacks/index.js
@@ -0,0 +1,9 @@
+const Stacks = () => {
+    return (  
+        <div>
+            <h1>All Stacks Professionals</h1>
+        </div>
+    );
+}
+ 
+export default Stacks;
